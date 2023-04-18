8b066901f8b08e630c9536a774f43cec3804ddc4
unknown <Amiram.ab@gmail.com>
2023-04-18 20:25:30 +0300

    fix: app import fixed

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-from gui_app.app import main
+from gui_app import app
 
 if __name__ == "__main__":
-  yaml_data = main(debug=False)
+  yaml_data = app.main(debug=False)
   print("Received YAML data:")
   print(yaml_data)
